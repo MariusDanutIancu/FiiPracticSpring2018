df67dfe6f54b19fba8a652173166cd58bcafd347
MariusDanutIancu <marius.danut94@gmail.com>
2018-03-18 21:40:07 +0200

    Added patient controller

---
+++ src/main/java/com/healthcare/main/controller/PatientController.java
@@ -0,0 +1,65 @@
+package com.healthcare.main.controller;
+
+import com.healthcare.main.model.Patient;
+import com.healthcare.main.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping(value = "/api/0.1/patients")
+public class PatientController
+{
+    private PatientService patientService;
+
+    @Autowired
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping(value="/{id}")
+    public Patient getPatient(@PathVariable Long id)
+    {
+        return patientService.getPatient(id);
+    }
+
+    @GetMapping
+    public List<Patient> getPatients()
+    {
+        return patientService.getAllPatients();
+    }
+
+    @PostMapping
+    public Patient savePatient(@RequestBody Patient patient)
+    {
+        patientService.savePatient(patient);
+        return patient;
+    }
+
+    @PutMapping(value="/{id}")
+    public Patient updatePatient(@RequestBody Patient patient)
+    {
+        return patientService.updatePatient(patient);
+    }
+
+//    @PutMapping
+//    public ResponseEntity<Object> updatePatients()
+//    {
+//        // doctorService.updatePatients(patientList);
+//        return new ResponseEntity<>(null, HttpStatus.NOT_IMPLEMENTED);
+//    }
+
+    @DeleteMapping(value="/{id}")
+    public Boolean deletePatient(@PathVariable Long id)
+    {
+        return patientService.deletePatient(id);
+    }
+
+    @DeleteMapping
+    public Boolean deleteAllPatients()
+    {
+        return patientService.deleteAllPatients();
+    }
+}
