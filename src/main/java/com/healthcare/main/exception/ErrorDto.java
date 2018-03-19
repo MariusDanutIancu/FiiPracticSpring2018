da86c5b31b2189c5218d68e2590be7f573b68a1b
MariusDanutIancu <marius.danut94@gmail.com>
2018-03-19 11:04:00 +0200

    Added ErrorDto

---
+++ src/main/java/com/healthcare/main/exception/ErrorDto.java
@@ -0,0 +1,31 @@
+package com.healthcare.main.exception;
+
+
+public class ErrorDto
+{
+    private int statusCode;
+    private String message;
+
+    public ErrorDto() {}
+
+    public ErrorDto(int statusCode, String message) {
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
