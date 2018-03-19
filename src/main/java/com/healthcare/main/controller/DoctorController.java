40ba0a9b27dcf83c42fde2606729ef599d4ce3b9
MariusDanutIancu <marius.danut94@gmail.com>
2018-03-19 12:06:59 +0200

    Exception management for DoctorController

--- src/main/java/com/healthcare/main/controller/DoctorController.java
+++ src/main/java/com/healthcare/main/controller/DoctorController.java
@@ -1,8 +1,12 @@
 package com.healthcare.main.controller;
 
+import com.healthcare.main.exception.BadRequestException;
+import com.healthcare.main.exception.NotFoundException;
+import com.healthcare.main.mapper.ObjectMapper;
 import com.healthcare.main.model.Doctor;
 import com.healthcare.main.service.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,9 +23,14 @@ public class DoctorController
     }
 
     @GetMapping(value="/{id}")
-    public Doctor getDoctor(@PathVariable Long id)
+    public Doctor getDoctor(@PathVariable("id") Long id) throws NotFoundException
     {
-        return doctorService.getDoctor(id);
+        Doctor doctor = doctorService.getDoctor(id);
+        if(doctor == null)
+        {
+            throw new NotFoundException(String.format("Doctor with id=%s was not found.", id));
+        }
+        return doctor;
     }
 
     @GetMapping
@@ -31,35 +40,43 @@ public class DoctorController
     }
 
     @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
     public Doctor saveDoctor(@RequestBody Doctor doctor)
     {
-        doctorService.saveDoctor(doctor);
-        return doctor;
+        return doctorService.saveDoctor(doctor);
     }
 
     @PutMapping(value="/{id}")
-    public Doctor updateDoctor(@RequestBody Doctor doctor)
+    public Doctor updateDoctor(@PathVariable("id") Long id, @RequestBody Doctor doctor) throws BadRequestException, NotFoundException
     {
+        if(!id.equals(doctor.getDoctorID())){
+            throw new BadRequestException("The id is not the same with id from object");
+        }
+
+        Doctor doctorDb = doctorService.getDoctor(id);
+        if(doctorDb == null){
+            throw new NotFoundException(String.format("Doctor with id=%s was not found.", id));
+        }
+
+        ObjectMapper.map2DoctorDb(doctorDb, doctor);
         return  doctorService.updateDoctor(doctor);
     }
 
-//    @PutMapping
-//    public ResponseEntity<Object> updateDoctors()
-//    {
-//        // doctorService.updateDoctors(doctor);
-//        return new ResponseEntity<>(null, HttpStatus.NOT_IMPLEMENTED);
-//    }
-
     @DeleteMapping(value="/{id}")
-    public Boolean deleteDoctor(@PathVariable Long id)
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deleteDoctor(@PathVariable Long id) throws NotFoundException
     {
-        Doctor doctor = doctorService.getDoctor(id);
-        return doctorService.deleteDoctor(doctor);
+        Doctor doctorDb  = doctorService.getDoctor(id);
+        if(doctorDb == null){
+            throw new NotFoundException(String.format("Doctor with id=%s was not found.", id));
+        }
+        doctorService.deleteDoctor(doctorDb);
     }
 
     @DeleteMapping
-    public Boolean deleteAllDoctors()
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deleteAllDoctors()
     {
-        return doctorService.deleteAllDoctors();
+        doctorService.deleteAllDoctors();
     }
 }
