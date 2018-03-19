38eeb0a8b70807370d4d39eb28f878aefe081b2c
MariusDanutIancu <marius.danut94@gmail.com>
2018-03-19 11:58:04 +0200

    Updated HealthDB

--- src/main/java/com/healthcare/main/database/HealthDB.java
+++ src/main/java/com/healthcare/main/database/HealthDB.java
@@ -18,34 +18,7 @@ public class HealthDB
      */
     public HealthDB(){
         this.doctors = new HashMap<>();
-
-        Doctor doctor_0 = new Doctor();
-        doctor_0.setDoctorID(1L);
-        doctor_0.setFirstName("name");
-        doctor_0.setSpecialization("function");
-
-        Doctor doctor_1 = new Doctor();
-        doctor_1.setDoctorID(2L);
-        doctor_1.setFirstName("name");
-        doctor_1.setSpecialization("function");
-
-        this.doctors.put(doctor_0.getDoctorID(), doctor_0);
-        this.doctors.put(doctor_1.getDoctorID(), doctor_1);
-
         this.patients = new HashMap<>();
-
-        Patient patient_0 = new Patient();
-        patient_0.setPatientID(1L);
-        patient_0.setFirstName("name");
-        patient_0.setLastName("name");
-
-        Patient patient_1 = new Patient();
-        patient_1.setPatientID(2L);
-        patient_1.setFirstName("name");
-        patient_1.setLastName("name");
-
-        this.patients.put(patient_0.getPatientID(), patient_0);
-        this.patients.put(patient_1.getPatientID(), patient_1);
     }
 
     /**
