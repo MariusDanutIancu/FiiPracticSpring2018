2cfa1bb5f63ed81e336006859f87c2425e827f5e
MariusDanutIancu <marius.danut94@gmail.com>
2018-03-22 21:05:41 +0200

    Added DoctorBatch

---
+++ src/main/java/com/healthcare/main/model/DoctorBatch.java
@@ -0,0 +1,27 @@
+package com.healthcare.main.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DoctorBatch
+{
+    private List<Doctor> doctorList;
+
+    public DoctorBatch()
+    {
+        this.doctorList = new ArrayList<>();
+    }
+
+    public DoctorBatch(List<Doctor> doctorList)
+    {
+        this.doctorList = doctorList;
+    }
+
+    public List<Doctor> getDoctorList() {
+        return doctorList;
+    }
+
+    public void setDoctorList(List<Doctor> doctorList) {
+        this.doctorList = doctorList;
+    }
+}
