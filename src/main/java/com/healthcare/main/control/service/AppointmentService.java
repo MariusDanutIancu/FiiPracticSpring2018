363b43c62600ff514b483987f7b6f5e59acb79e2
MariusDanutIancu <marius.danut94@gmail.com>
2018-03-28 15:34:47 +0300

    Removed appointment service and repository

--- src/main/java/com/healthcare/main/control/service/AppointmentService.java
+++
@@ -1,9 +0,0 @@
-package com.healthcare.main.control.service;
-
-import com.healthcare.main.entity.model.Appointment;
-
-public interface AppointmentService {
-
-    Appointment getAppointment(Long id);
-    Appointment saveAppointment(Appointment doctor);
-}
--- src/main/java/com/healthcare/main/entity/repository/AppointmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.healthcare.main.entity.repository;
-
-import com.healthcare.main.entity.model.Appointment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
-}
