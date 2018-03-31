cd5c9268b08064fe67d9f4d72f1654315f47fc7e
MariusDanutIancu <marius.danut94@gmail.com>
2018-03-31 19:43:33 +0300

    Errordto refactoring and initial docstrings

--- src/main/java/com/healthcare/main/boundry/errorHandling/ErrorDto.java
+++ src/main/java/com/healthcare/main/boundry/errorHandling/ErrorDto.java
@@ -1,6 +1,8 @@
 package com.healthcare.main.boundry.errorHandling;
 
-
+/**
+ *
+ */
 public class ErrorDto
 {
     private int statusCode;
@@ -8,23 +10,44 @@ public class ErrorDto
 
     public ErrorDto() {}
 
+    /**
+     *
+     * @param statusCode
+     * @param message
+     */
     public ErrorDto(int statusCode, String message) {
         this.statusCode = statusCode;
         this.message = message;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getStatusCode() {
         return statusCode;
     }
 
+    /**
+     *
+     * @param statusCode
+     */
     public void setStatusCode(int statusCode) {
         this.statusCode = statusCode;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getMessage() {
         return message;
     }
 
+    /**
+     *
+     * @param message
+     */
     public void setMessage(String message) {
         this.message = message;
     }
