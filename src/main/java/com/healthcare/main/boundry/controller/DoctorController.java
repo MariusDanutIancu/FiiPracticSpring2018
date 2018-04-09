e7e7939aed34e6e70204a91e75ead76772997423
MariusDanutIancu <marius.danut94@gmail.com>
2018-04-09 21:34:31 +0300

    * adapted other doctor controller requests to support dtos * updated doctor mapper

--- src/main/java/com/healthcare/main/boundry/controller/DoctorController.java
+++ src/main/java/com/healthcare/main/boundry/controller/DoctorController.java
@@ -1,6 +1,8 @@
 package com.healthcare.main.boundry.controller;
 
 import com.healthcare.main.boundry.dto.DoctorDto;
+import com.healthcare.main.boundry.exception.BadRequestException;
+import com.healthcare.main.boundry.exception.MethodNotAllowedException;
 import com.healthcare.main.boundry.exception.NotFoundException;
 import com.healthcare.main.boundry.mapper.DoctorMapper;
 import com.healthcare.main.entity.model.Doctor;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/api/0.1/doctors")
@@ -37,31 +41,35 @@ public class DoctorController
     public DoctorDto getDoctor(@PathVariable("id") Long id) throws NotFoundException
     {
         Doctor doctor = doctorService.getDoctor(id);
-
         if(doctor == null)
         {
             throw new NotFoundException(String.format("Doctor with id=%s was not found.", id));
         }
-
         return DoctorMapper.MAPPER.fromDoctor(doctor);
     }
-//
-//    /**
-//     *
-//     * @return
-//     * @throws NotFoundException
-//     */
-//    @GetMapping
-//    public List<Doctor> getDoctors() throws NotFoundException
-//    {
-//        List<Doctor> doctorListDb = doctorService.getAllDoctors();
-//        if(doctorListDb.size() == 0)
-//        {
-//            throw new NotFoundException("There are no doctors in the database.");
-//        }
-//        return doctorListDb;
-//    }
-//
+
+    /**
+     *
+     * @return
+     * @throws NotFoundException
+     */
+    @GetMapping
+    public List<DoctorDto> getDoctors() throws NotFoundException
+    {
+        List<Doctor> doctorListDb = doctorService.getAllDoctors();
+        if(doctorListDb.size() == 0)
+        {
+            throw new NotFoundException("There are no doctors in the database.");
+        }
+
+        List<DoctorDto> doctorDtos = new ArrayList<>();
+        for(Doctor doctor:doctorListDb)
+        {
+            doctorDtos.add(DoctorMapper.MAPPER.fromDoctor(doctor));
+        }
+        return doctorDtos;
+    }
+
     /**
      *
      * @param doctorDto
@@ -76,70 +84,72 @@ public class DoctorController
         //this.sendEmail(doctor);
         return DoctorMapper.MAPPER.fromDoctor(doctor);
     }
-//
-//    /**
-//     *
-//     * @param id
-//     * @param doctor
-//     * @return
-//     * @throws MethodNotAllowedException
-//     */
-//    @PostMapping(value="/{id}")
-//    public Doctor saveDoctor_not_allowed(@PathVariable("id") Long id, @RequestBody Doctor doctor) throws MethodNotAllowedException
-//    {
-//        throw new MethodNotAllowedException("Method is not allowed.");
-//    }
-//
-//    /**
-//     *
-//     * @param id
-//     * @param doctor
-//     * @return
-//     * @throws BadRequestException
-//     * @throws NotFoundException
-//     */
-//    @PutMapping(value="/{id}")
-//    public Doctor updateDoctor(@PathVariable("id") Long id, @RequestBody Doctor doctor) throws BadRequestException, NotFoundException
-//    {
-//        if(!id.equals(doctor.getDoctorID())){
-//            throw new BadRequestException("The id is not the same with id from object");
-//        }
-//
-//        Doctor doctorDb = doctorService.getDoctor(id);
-//        if(doctorDb == null){
-//            throw new NotFoundException(String.format("Doctor with id=%s was not found.", id));
-//        }
-//
-//        ObjectMapper.map2DoctorDb(doctorDb, doctor);
-//        return  doctorService.updateDoctor(doctorDb);
-//    }
-//
-//    /**
-//     *
-//     * @param id
-//     * @throws NotFoundException
-//     */
-//    @DeleteMapping(value="/{id}")
-//    @ResponseStatus(value = HttpStatus.NO_CONTENT)
-//    public void deleteDoctor(@PathVariable Long id) throws NotFoundException
-//    {
-//        Doctor doctorDb  = doctorService.getDoctor(id);
-//        if(doctorDb == null){
-//            throw new NotFoundException(String.format("Doctor with id=%s was not found.", id));
-//        }
-//        doctorService.deleteDoctor(doctorDb);
-//    }
-//
-//    /**
-//     *
-//     */
-//    @DeleteMapping
-//    @ResponseStatus(value = HttpStatus.NO_CONTENT)
-//    public void deleteAllDoctors()
-//    {
-//        doctorService.deleteAllDoctors();
-//    }
-//
+
+    /**
+     *
+     * @param id
+     * @param doctor
+     * @return
+     * @throws MethodNotAllowedException
+     */
+    @PostMapping(value="/{id}")
+    public Doctor saveDoctor_not_allowed(@PathVariable("id") Long id, @RequestBody Doctor doctor) throws MethodNotAllowedException
+    {
+        throw new MethodNotAllowedException("Method is not allowed.");
+    }
+
+    /**
+     *
+     * @param id
+     * @param doctorDto
+     * @return
+     * @throws BadRequestException
+     * @throws NotFoundException
+     */
+    @PutMapping(value="/{id}")
+    public DoctorDto updateDoctor(@PathVariable("id") Long id, @RequestBody DoctorDto doctorDto) throws BadRequestException, NotFoundException
+    {
+        if(!id.equals(doctorDto.getDoctor_id())){
+            throw new BadRequestException("The id is not the same with id from object");
+        }
+
+        Doctor doctorDb = doctorService.getDoctor(id);
+        if(doctorDb == null){
+            throw new NotFoundException(String.format("Doctor with id=%s was not found.", id));
+        }
+
+        DoctorMapper.MAPPER.toDoctor(doctorDto, doctorDb);
+        doctorDb = doctorService.updateDoctor(doctorDb);
+
+        return DoctorMapper.MAPPER.fromDoctor(doctorDb);
+    }
+
+    /**
+     *
+     * @param id
+     * @throws NotFoundException
+     */
+    @DeleteMapping(value="/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deleteDoctor(@PathVariable Long id) throws NotFoundException
+    {
+        Doctor doctorDb  = doctorService.getDoctor(id);
+        if(doctorDb == null){
+            throw new NotFoundException(String.format("Doctor with id=%s was not found.", id));
+        }
+        doctorService.deleteDoctor(doctorDb);
+    }
+
+    /**
+     *
+     */
+    @DeleteMapping
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deleteAllDoctors()
+    {
+        doctorService.deleteAllDoctors();
+    }
+
     /**
      *
      * @param doctor
--- src/main/java/com/healthcare/main/boundry/mapper/DoctorMapper.java
+++ src/main/java/com/healthcare/main/boundry/mapper/DoctorMapper.java
@@ -4,6 +4,7 @@ import com.healthcare.main.boundry.dto.DoctorDto;
 import com.healthcare.main.entity.model.Doctor;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
@@ -34,4 +35,15 @@ public interface DoctorMapper {
             @Mapping(source = "email.email", target = "email.email"),
     })
     Doctor toDoctor(DoctorDto doctorDto);
+
+    @Mappings({
+            @Mapping(source = "doctor_id", target = "id"),
+            @Mapping(source = "firstName", target = "firstName"),
+            @Mapping(source = "lastName", target = "lastName"),
+            @Mapping(source = "phoneNumber.phoneNumber", target = "phoneNumber.phoneNumber"),
+            @Mapping(source = "function", target = "function"),
+            @Mapping(source = "address.street", target = "address.street"),
+            @Mapping(source = "email.email", target = "email.email"),
+    })
+    void toDoctor(DoctorDto doctorDto, @MappingTarget Doctor doctorEntity);
 }
