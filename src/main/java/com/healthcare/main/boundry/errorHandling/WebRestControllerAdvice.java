ef3fc20137473448386b394270f499cd8d9ad234
MariusDanutIancu <marius.danut94@gmail.com>
2018-04-16 15:26:03 +0300

    Issue #5: Logger has been configured for console. Updated application.properties with logger configuration. Updated RestControllerAdvice to support error logging.

--- src/main/java/com/healthcare/main/boundry/errorHandling/WebRestControllerAdvice.java
+++ src/main/java/com/healthcare/main/boundry/errorHandling/WebRestControllerAdvice.java
@@ -4,6 +4,8 @@ package com.healthcare.main.boundry.errorHandling;
 import com.healthcare.main.boundry.exception.BadRequestException;
 import com.healthcare.main.boundry.exception.MethodNotAllowedException;
 import com.healthcare.main.boundry.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -12,10 +14,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class WebRestControllerAdvice
 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebRestControllerAdvice.class);
+
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     public ErrorDto handleNotFoundException(NotFoundException ex)
     {
+        LOGGER.error("NOT_FOUND error encountered in {} : {}", ex.getStackTrace()[0], ex.getMessage());
         return this.generateErrorDto(HttpStatus.NOT_FOUND, ex);
     }
 
@@ -23,6 +28,7 @@ public class WebRestControllerAdvice
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     private ErrorDto handleBadRequestException(BadRequestException ex)
     {
+        LOGGER.error("BAD_REQUEST error encountered in {} : {}", ex.getStackTrace()[0], ex.getMessage());
         return this.generateErrorDto(HttpStatus.BAD_REQUEST, ex);
     }
 
@@ -30,6 +36,7 @@ public class WebRestControllerAdvice
     @ResponseStatus(value = HttpStatus.METHOD_NOT_ALLOWED)
     private ErrorDto handleMethodNotAllowedException(MethodNotAllowedException ex)
     {
+        LOGGER.error("METHOD_NOT_ALLOWED error encountered in {} : {}", ex.getStackTrace()[0], ex.getMessage());
         return this.generateErrorDto(HttpStatus.METHOD_NOT_ALLOWED, ex);
     }
 
@@ -37,6 +44,7 @@ public class WebRestControllerAdvice
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
     private ErrorDto defaultHandler(Exception ex)
     {
+        LOGGER.error("INTERNAL_SERVER_ERROR error encountered in {} : {}", ex.getStackTrace()[0], ex.getMessage());
         return new ErrorDto(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage());
     }
 
