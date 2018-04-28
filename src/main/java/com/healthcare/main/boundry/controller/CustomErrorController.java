13848015292d699df825efc1e9bbaca0f52e09ad
MariusDanutIancu <marius.danut94@gmail.com>
2018-04-28 21:05:54 +0300

    Added CustomErrorController

---
+++ src/main/java/com/healthcare/main/boundry/controller/CustomErrorController.java
@@ -0,0 +1,39 @@
+package com.healthcare.main.boundry.controller;
+
+import com.healthcare.main.boundry.errorHandling.ErrorDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.ServletWebRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+@RestController
+public class CustomErrorController implements ErrorController {
+
+    private static final String PATH = "/error";
+
+    @Autowired
+    private ErrorAttributes errorAttributes;
+
+    @GetMapping(value = PATH)
+    public ErrorDto error(HttpServletRequest request, HttpServletResponse response)
+    {
+        return new ErrorDto(response.getStatus(), (String)getErrorAttributes(request, true).get("message"));
+    }
+
+    @Override
+    public String getErrorPath()
+    {
+        return PATH;
+    }
+
+    private Map<String, Object> getErrorAttributes(HttpServletRequest request, boolean includeStackTrace) {
+        ServletWebRequest servletWebRequest = new ServletWebRequest(request);
+        return errorAttributes.getErrorAttributes(servletWebRequest, includeStackTrace);
+    }
+}
