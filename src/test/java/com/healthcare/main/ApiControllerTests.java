c388d18317eb92acc29b4e4883f3c1b1bf9da96e
MariusDanutIancu <marius.danut94@gmail.com>
2018-04-28 16:33:43 +0300

    * Added ApiControlleTests * Added not null test case for api controller getInfo request

---
+++ src/test/java/com/healthcare/main/ApiControllerTests.java
@@ -0,0 +1,34 @@
+package com.healthcare.main;
+
+import com.healthcare.main.boundry.controller.ApiController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ApiController.class)
+public class ApiControllerTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void whenGetApiInfo_thenReturnJson() throws Exception {
+
+        mvc.perform(MockMvcRequestBuilders.get("/")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$.requestPathTemplate", notNullValue()))
+                .andExpect(jsonPath("$.currentVersion", notNullValue()))
+                .andExpect(jsonPath("$.description", notNullValue()));
+    }
+}
