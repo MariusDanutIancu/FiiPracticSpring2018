cd8d98c7d9b10a16f0244d40e88595d20dbf617e
MariusDanutIancu <marius.danut94@gmail.com>
2018-05-02 00:20:10 +0300

    * refactored pom * removed logging from application.properties * added logback-spring.xml * added logging to MarkAppointment

--- src/main/java/com/healthcare/main/control/tasks/MarkAppointment.java
+++ src/main/java/com/healthcare/main/control/tasks/MarkAppointment.java
@@ -2,6 +2,8 @@ package com.healthcare.main.control.tasks;
 
 import com.healthcare.main.control.service.AppointmentService;
 import com.healthcare.main.entity.model.Appointment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -14,20 +16,30 @@ public class MarkAppointment {
 
     private AppointmentService appointmentService;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MarkAppointment.class);
+
     @Autowired
     public MarkAppointment(AppointmentService appointmentService) {
         this.appointmentService = appointmentService;
     }
 
     @Scheduled(fixedDelay = 300000)
-    public void reportCurrentTime() {
+    public void markAppointments() {
+
+        LOGGER.info("Marking appointments started");
+
         Date current_date = new Date();
+        Long counter = 0L;
         List<Appointment> appointmentList = appointmentService.findAllByEndTimeLessThanEqualAndTookPlace(current_date, false);
 
         for(Appointment appointment:appointmentList)
         {
             appointment.setTookPlace(true);
             appointmentService.saveAppointment(appointment);
+            counter++;
+            LOGGER.info("Marked appointment with id {}", appointment.getId());
         }
+
+        LOGGER.info("Marked {} appointments", counter);
     }
 }
