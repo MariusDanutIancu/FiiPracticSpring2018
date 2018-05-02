824e43c9169a8121e71b0f968ada12efa6621199
MariusDanutIancu <marius.danut94@gmail.com>
2018-05-02 17:09:18 +0300

    Refactored CustomErrorController

--- src/main/java/com/healthcare/main/boundry/controller/CustomErrorController.java
+++ src/main/java/com/healthcare/main/boundry/controller/CustomErrorController.java
@@ -16,22 +16,42 @@ import java.util.Map;
 public class CustomErrorController implements ErrorController {
 
     private static final String PATH = "/error";
-
-    @Autowired
     private ErrorAttributes errorAttributes;
 
+    @Autowired
+    public CustomErrorController(ErrorAttributes errorAttributes)
+    {
+        this.errorAttributes = errorAttributes;
+    }
+
+    /**
+     *
+     * @param request
+     * @param response
+     * @return
+     */
     @GetMapping(value = PATH)
     public ErrorDto error(HttpServletRequest request, HttpServletResponse response)
     {
         return new ErrorDto(response.getStatus(), (String)getErrorAttributes(request, true).get("message"));
     }
 
+    /**
+     *
+     * @return
+     */
     @Override
     public String getErrorPath()
     {
         return PATH;
     }
 
+    /**
+     *
+     * @param request
+     * @param includeStackTrace
+     * @return
+     */
     private Map<String, Object> getErrorAttributes(HttpServletRequest request, boolean includeStackTrace) {
         ServletWebRequest servletWebRequest = new ServletWebRequest(request);
         return errorAttributes.getErrorAttributes(servletWebRequest, includeStackTrace);
