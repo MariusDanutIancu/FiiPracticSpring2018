4fc89b9a58aa45870fd8e8010c51d1a86f950e12
MariusDanutIancu <marius.danut94@gmail.com>
2018-05-03 14:39:50 +0300

    Updated apiControllerTests

--- src/test/java/com/healthcare/main/ApiControllerTests.java
+++ src/test/java/com/healthcare/main/ApiControllerTests.java
@@ -2,35 +2,48 @@ package com.healthcare.main;
 
 import com.healthcare.main.boundry.controller.ApiController;
 import com.healthcare.main.properties.ApiProperties;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.Matchers.notNullValue;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-//@RunWith(SpringRunner.class)
-//@WebMvcTest(ApiController.class)
-//public class ApiControllerTests {
-//
-//    @Autowired
-//    private MockMvcc;
-//
-//    @Test
-//    public void whenGetApiInfo_thenReturnJson() throws Exception {
-//
-////        mvc.perform(MockMvcRequestBuilders.get("/")
-////                .contentType(MediaType.APPLICATION_JSON))
-////                .andExpect(MockMvcResultMatchers.status().isOk());
-////
-////                .andExpect(jsonPath("$.requestPathTemplate", notNullValue()))
-////                .andExpect(jsonPath("$.currentVersion", notNullValue()))
-////                .andExpect(jsonPath("$.description", notNullValue()));
-//    }
-//}
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class ApiControllerTests {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .build();
+    }
+
+    @Test
+    public void whenGetApiInfo_thenReturnJson() throws Exception {
+
+        mvc.perform(MockMvcRequestBuilders.get("/")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$.requestPathTemplate", notNullValue()))
+                .andExpect(jsonPath("$.currentVersion", notNullValue()))
+                .andExpect(jsonPath("$.description", notNullValue()));
+    }
+}
